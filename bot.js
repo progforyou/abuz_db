f94884d3413c918d2d301ffc40ed0836ca5cea2a
nikolay <nikolay.sychev.1999@gmail.com>
2022-08-06 18:27:33 +0300

    fix

--- bot.js
+++ bot.js
@@ -1,111 +1,33 @@
 const Telegraf = require('telegraf');
-const {createUser, getUser, extendLicense, createUserRat, getAllUsers, deleteUser} = require("./db_tools");
 //https://github.com/telegraf/telegraf/issues/705
-/*const start = require("./controllers/start");
-const getUser = require("./controllers/getUser");
+const ExtendLicenseScene = require("./scenes/extendLicense");
+const start = require("./scenes/start");
+const MenuScene = require("./scenes/menu");
+const CreateUserScene = require("./scenes/createUser");
+const EditUserScene = require("./scenes/editUser");
+const DeleteUserScene = require("./scenes/deleteUser");
+const DataMenuScene = require("./scenes/dataMenu");
+const FindUserScene = require("./scenes/findUser");
 
 
 const stage = new Telegraf.Scenes.Stage([
     start,
-    getUser
-]);*/
+    MenuScene,
+    CreateUserScene,
+    EditUserScene,
+    DeleteUserScene,
+    DataMenuScene,
+    FindUserScene,
+    ExtendLicenseScene
+]);
 
-/*bot.use(stage.middleware());*/
-const rootIds = [494127139, 473018697]
 const bot = new Telegraf.Telegraf(process.env.TELEGRAM_API_TOKEN);
+bot.use(Telegraf.session())
+bot.use(stage.middleware());
 
-bot.command('start', ctx => {
-    if (rootIds.includes(ctx.from.id)) {
-        ctx.reply('hello boss! Enter command...')
-    } else {
-        ctx.reply('You are not a boss! Bye!')
-    }
-})
+bot.command('start',(ctx) => ctx.scene.enter('start'));
 
-bot.command('createUser', ctx => {
-    if (!rootIds.includes(ctx.from.id)) {
-        ctx.reply('You are not a boss! Bye!')
-        return
-    }
-    let name = Math.random().toString(36).slice(-8);
-    let password = Math.random().toString(36).slice(-8);
-    //create in db
-    createUser(name, password).then(() =>{
-        ctx.reply(`User created!\n name: ${name} \n password: ${password}`)  
-    })
-})
-
-bot.command('createUserRat', ctx => {
-    if (!rootIds.includes(ctx.from.id)) {
-        ctx.reply('You are not a boss! Bye!')
-        return
-    }
-    let name = Math.random().toString(36).slice(-8);
-    let password = Math.random().toString(36).slice(-8);
-    //create in db
-    createUserRat(name, password).then(() =>{
-        ctx.reply(`User created!\n name: ${name} \n password: ${password}`)
-    })
-})
-
-bot.command('getAllUsers', ctx => {
-    if (!rootIds.includes(ctx.from.id)) {
-        ctx.reply('You are not a boss! Bye!')
-        return
-    }
-    //create in db
-    getAllUsers().then((r) =>{
-        if (r) ctx.reply(`Found users !\n ${JSON.stringify(r)}`)
-        else ctx.reply(`User not found!`)
-    }).catch(e => {
-        ctx.reply(`Error get user!`)
-    })
-})
-
-
-
-
-
-bot.on('text', (ctx) => {
-    if (!rootIds.includes(ctx.from.id)) {
-        ctx.reply('You are not a boss! Bye!')
-        return
-    }
-    if (ctx.message.text.split(" ").length !== 3){
-        ctx.reply("Bad command")
-        return
-    }
-    let func = ctx.message.text.split(" ")[0]
-    let name = ctx.message.text.split(" ")[1]
-    let password = ctx.message.text.split(" ")[2]
-    switch (func) {
-        case "get":
-            getUser(name, password).then((r) =>{
-                if (r) ctx.reply(`Found user !\n ${JSON.stringify(r)}`)
-                else ctx.reply(`User not found!`)
-            }).catch(e => {
-                ctx.reply(`Error get user!`)
-            })
-            break;
-        case "extend":
-            extendLicense(name, password).then((r) =>{
-                if (r) ctx.reply(`License extended!`)
-                else ctx.reply(`User not found!`)
-            }).catch(e => {
-                ctx.reply(`Error extended!`)
-            })
-            break;
-        case "delete":
-            deleteUser(name, password).then((r) =>{
-                if (r) ctx.reply(`User deleted!`)
-                else ctx.reply(`User not found!`)
-            }).catch(e => {
-                ctx.reply(`Error extended!`)
-            })
-            break;
-    }
-
-});
+bot.command('menu', (ctx) => ctx.scene.enter('menu'))
 
 
 
